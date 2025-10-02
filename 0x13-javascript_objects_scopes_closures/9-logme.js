dc64fc934e761afcc4eb21f4b431cad1ab34bc7d
aliibrn <alibrne53@gmail.com>
2025-10-02 18:40:09 +0100

    the number of arguments already printed and the new argument value --print

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,7 @@
+exports.logMe = function (item) {
+    if (exports.logMe.count === undefined) {
+        exports.logMe.count = 0; 
+    }
+    console.log(`${exports.logMe.count}: ${item}`);
+    exports.logMe.count += 1;  
+}
